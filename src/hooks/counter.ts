6265b558561ad08630ce23dc8943b9b0a37d3552
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-03 15:05:14 +0100

    added the increment & decrement callback for the useCounter hook

--- src/hooks/counter.ts
+++ src/hooks/counter.ts
@@ -1,7 +1,22 @@
+import { useCallback } from "react";
 import { Signal, useSignal } from ".";
 
 export const counterSignal = new Signal(0);
 
 export const useCounter = () => {
-  return useSignal(counterSignal);
+  const counter = useSignal(counterSignal);
+
+  const increment = useCallback(() => {
+    counterSignal.next(oldCounter => oldCounter + 1);
+  }, []);
+
+  const decrement = useCallback(() => {
+    counterSignal.next(oldCounter => oldCounter - 1);
+  }, []);
+
+  return {
+    increment,
+    decrement,
+    counter
+  };
 };
