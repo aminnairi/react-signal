3e04042fa7d2fb8ec2f50e76684eba295761a11b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-03 13:24:10 +0100

    added a new route to the theme page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
 import { Header } from "./components/Header";
 import { Fragment } from "react";
+import { ThemePage } from "./pages/ThemePage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/theme" element={<ThemePage />} />
       </Routes>
     </Fragment>
   );
