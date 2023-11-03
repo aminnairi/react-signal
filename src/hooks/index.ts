5e8932fd13115494df9b7200c6909b6d879c4be0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-03 13:23:56 +0100

    added signals for storage, local & session storages

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,19 @@ import { useCallback, useSyncExternalStore } from "react";
 
 export type Subscriber = () => void;
 
+export type Validation<Value> = (value: unknown) => value is Value;
+
+export type StorageSignalConstructor<Value> = {
+  storage: Storage,
+  key: string,
+  value: Value,
+  validation: Validation<Value>
+}
+
+export type LocalStorageSignalConstructor<Value> = Omit<StorageSignalConstructor<Value>, "storage">
+
+export type SessionStorageSignalConstructor<Value> = Omit<StorageSignalConstructor<Value>, "storage">
+
 export class Signal<Value> {
   private subscribers: Array<Subscriber>;
   private value: Value;
@@ -30,6 +43,59 @@ export class Signal<Value> {
       const foundSubscriberIndex = this.subscribers.findIndex(subscriber => subscriber === newSubscriber);
       this.subscribers.splice(foundSubscriberIndex, 1);
     }
+  }
+}
+
+export class StorageSignal<Value> extends Signal<Value> {
+  private key: string;
+  private storage: Storage;
+
+  public constructor({ storage, key, value, validation }: StorageSignalConstructor<Value>) {
+    try {
+      const storageValue = JSON.parse(storage.getItem(key) || "");
+
+      if (validation(storageValue)) {
+        super(storageValue);
+      } else {
+        super(value);
+      }
+    } catch {
+      super(value);
+    }
+
+    this.key = key;
+    this.storage = storage;
+  }
+
+  public override emit(newValue: Value): void {
+    this.storage.setItem(this.key, JSON.stringify(newValue));
+    super.emit(newValue);
+  }
+
+  public remove(): void {
+    this.storage.removeItem(this.key);
+  }
+}
+
+export class LocalStorageSignal<Value> extends StorageSignal<Value> {
+  public constructor({ key, value, validation }: LocalStorageSignalConstructor<Value>) {
+    super({
+      storage: localStorage,
+      key,
+      value,
+      validation
+    });
+  }
+}
+
+export class SessionStorageSignal<Value> extends StorageSignal<Value> {
+  public constructor({ key, value, validation }: StorageSignalConstructor<Value>) {
+    super({
+      storage: sessionStorage,
+      key,
+      value,
+      validation
+    });
   }
 }
 
