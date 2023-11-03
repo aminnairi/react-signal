aa7d97201bb6d8ed42a4824e1f1e08a2ae5b8daa
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-03 20:06:55 +0100

    added a route to the user page in the header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AboutPage } from "./pages/AboutPage";
 import { Header } from "./components/Header";
 import { Fragment } from "react";
 import { ThemePage } from "./pages/ThemePage";
+import { UserPage } from "./pages/UserPage";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/theme" element={<ThemePage />} />
+        <Route path="/user" element={<UserPage />} />
       </Routes>
     </Fragment>
   );
